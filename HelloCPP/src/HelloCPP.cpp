a173404815faa11130a304249c364e53802d5880
kurakurara <k.kura.kurara@gmail.com>
2012-12-04 10:56:13 +0900

    Commit2012120403

--- HelloCPP/src/HelloCPP.cpp
+++ HelloCPP/src/HelloCPP.cpp
@@ -10,6 +10,6 @@
 using namespace std;
 
 int main() {
-	cout << "Hello, CPP World!!" << endl; // prints !!!Hello World!!!
+	cout << "Hello, CPP World!" << endl; // prints !!!Hello World!!!
 	return 0;
 }
